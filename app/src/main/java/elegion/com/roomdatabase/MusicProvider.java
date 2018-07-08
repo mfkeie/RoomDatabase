3a59bd729d109ec5675e48177db2c3997ab77fd5
urakov.ivan <urakov.ivan@gmail.com>
2018-07-08 22:03:02 +0300

    first commit

---
+++ app/src/main/java/elegion/com/roomdatabase/MusicProvider.java
@@ -0,0 +1,225 @@
+package elegion.com.roomdatabase;
+
+import android.arch.persistence.room.Room;
+import android.content.ContentProvider;
+import android.content.ContentUris;
+import android.content.ContentValues;
+import android.content.UriMatcher;
+import android.database.Cursor;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+
+import elegion.com.roomdatabase.database.Album;
+import elegion.com.roomdatabase.database.AlbumSong;
+import elegion.com.roomdatabase.database.MusicDao;
+import elegion.com.roomdatabase.database.MusicDatabase;
+import elegion.com.roomdatabase.database.Song;
+
+public class MusicProvider extends ContentProvider {
+
+    private static final String TAG = MusicProvider.class.getSimpleName();
+
+    private static final String AUTHORITY = "com.elegion.roomdatabase.musicprovider";
+
+    private static final String TABLE_ALBUM = "album";
+    private static final String TABLE_SONG = "song";
+    private static final String TABLE_ALBUM_SONG = "albumsong";
+
+    private static final UriMatcher URI_MATCHER = new UriMatcher(UriMatcher.NO_MATCH);
+
+    private static final int ALBUM_TABLE_CODE = 100;
+    private static final int ALBUM_ROW_CODE = 101;
+
+    private static final int SONG_TABLE_CODE = 200;
+    private static final int SONG_ROW_CODE = 201;
+
+    private static final int ALBUM_SONG_TABLE_CODE = 300;
+    private static final int ALBUM_SONG_ROW_CODE = 301;
+
+    static {
+        URI_MATCHER.addURI(AUTHORITY, TABLE_ALBUM, ALBUM_TABLE_CODE);
+        URI_MATCHER.addURI(AUTHORITY, TABLE_ALBUM + "/*", ALBUM_ROW_CODE);
+
+        URI_MATCHER.addURI(AUTHORITY, TABLE_SONG, SONG_TABLE_CODE);
+        URI_MATCHER.addURI(AUTHORITY, TABLE_SONG + "/*", SONG_ROW_CODE);
+
+        URI_MATCHER.addURI(AUTHORITY, TABLE_ALBUM_SONG, ALBUM_SONG_TABLE_CODE);
+        URI_MATCHER.addURI(AUTHORITY, TABLE_ALBUM_SONG + "/*", ALBUM_SONG_ROW_CODE);
+    }
+
+    private MusicDao mMusicDao;
+
+    public MusicProvider() {
+    }
+
+    @Override
+    public boolean onCreate() {
+        if (getContext() != null) {
+            mMusicDao = Room.databaseBuilder(getContext().getApplicationContext(), MusicDatabase.class, "music_database")
+                    .build()
+                    .getMusicDao();
+            return true;
+        }
+
+        return false;
+    }
+
+    @Override
+    public String getType(Uri uri) {
+        switch (URI_MATCHER.match(uri)) {
+            case ALBUM_TABLE_CODE:
+                return "vnd.android.cursor.dir/" + AUTHORITY + "." + TABLE_ALBUM;
+            case ALBUM_ROW_CODE:
+                return "vnd.android.cursor.item/" + AUTHORITY + "." + TABLE_ALBUM;
+
+            case SONG_TABLE_CODE:
+                return "vnd.android.cursor.dir/" + AUTHORITY + "." + TABLE_SONG;
+            case SONG_ROW_CODE:
+                return "vnd.android.cursor.item/" + AUTHORITY + "." + TABLE_SONG;
+
+            case ALBUM_SONG_TABLE_CODE:
+                return "vnd.android.cursor.dir/" + AUTHORITY + "." + TABLE_ALBUM_SONG;
+            case ALBUM_SONG_ROW_CODE:
+                return "vnd.android.cursor.item/" + AUTHORITY + "." + TABLE_ALBUM_SONG;
+
+            default:
+                throw new UnsupportedOperationException("not yet implemented");
+        }
+    }
+
+    @Override
+    public Cursor query(Uri uri, String[] projection, String selection,
+                        String[] selectionArgs, String sortOrder) {
+
+        int code = URI_MATCHER.match(uri);
+
+        if (code != ALBUM_ROW_CODE && code != ALBUM_TABLE_CODE
+                && code != SONG_ROW_CODE && code != SONG_TABLE_CODE
+                && code != ALBUM_SONG_ROW_CODE && code != ALBUM_SONG_TABLE_CODE) return null;
+
+
+        switch (code) {
+            case ALBUM_TABLE_CODE:
+                return mMusicDao.getAlbumsCursor();
+            case ALBUM_ROW_CODE:
+                return mMusicDao.getAlbumWithIdCursor((int) ContentUris.parseId(uri));
+
+            case SONG_TABLE_CODE:
+                return mMusicDao.getSongsCursor();
+            case SONG_ROW_CODE:
+                return mMusicDao.getSongsCursorWithIdCursor((int) ContentUris.parseId(uri));
+
+            case ALBUM_SONG_TABLE_CODE:
+                return mMusicDao.getAlbumSongsCursor();
+            case ALBUM_SONG_ROW_CODE:
+                return mMusicDao.getAlbumSongsCursorWithIdCursor((int) ContentUris.parseId(uri));
+
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public Uri insert(@NonNull Uri uri, ContentValues values) {
+
+        if(isValuesValid(values)) {
+            switch (URI_MATCHER.match(uri)) {
+                case ALBUM_TABLE_CODE:
+                    Album album = new Album();
+                    Integer idAlbum = values.getAsInteger("id");
+                    album.setId(idAlbum);
+                    album.setName(values.getAsString("name"));
+                    album.setReleaseDate(values.getAsString("release"));
+                    mMusicDao.insertAlbum(album);
+                    return ContentUris.withAppendedId(uri, idAlbum);
+
+                case SONG_TABLE_CODE:
+                    Song song = new Song();
+                    Integer idSong = values.getAsInteger("id");
+                    song.setId(idSong);
+                    song.setName(values.getAsString("name"));
+                    song.setDuration(values.getAsString("duration"));
+                    mMusicDao.insertSong(song);
+                    return ContentUris.withAppendedId(uri, idSong);
+
+                case ALBUM_SONG_TABLE_CODE:
+                    AlbumSong albumSong = new AlbumSong();
+                    Integer idAlbumSong = values.getAsInteger("id");
+                    albumSong.setId(idAlbumSong);
+                    albumSong.setAlbumId(values.getAsInteger("albumId"));
+                    albumSong.setSongId(values.getAsInteger("songId"));
+                    mMusicDao.setLinkAlbumSongs(albumSong);
+                    return ContentUris.withAppendedId(uri, idAlbumSong);
+
+                default:
+                    throw new IllegalArgumentException("cant add multiple items");
+            }
+        } else {
+            throw new IllegalArgumentException("cant add multiple items");
+        }
+    }
+
+    @Override
+    public int update(@NonNull Uri uri, ContentValues values, String selection, String[] selectionArgs) {
+
+        if(isValuesValid(values)) {
+
+            switch (URI_MATCHER.match(uri)) {
+                case ALBUM_ROW_CODE:
+                    Album album = new Album();
+                    int idAlbum = (int) ContentUris.parseId(uri);
+                    album.setId(idAlbum);
+                    album.setName(values.getAsString("name"));
+                    album.setReleaseDate(values.getAsString("release"));
+                    return mMusicDao.updateAlbumInfo(album);
+
+                case SONG_ROW_CODE:
+                    Song song = new Song();
+                    int id = (int) ContentUris.parseId(uri);
+                    song.setId(id);
+                    song.setName(values.getAsString("name"));
+                    song.setDuration(values.getAsString("duration"));
+                    return mMusicDao.updateSongInfo(song);
+
+                case ALBUM_SONG_ROW_CODE:
+                    AlbumSong albumSong = new AlbumSong();
+                    int idAlbumSong = (int) ContentUris.parseId(uri);
+                    albumSong.setId(idAlbumSong);
+                    albumSong.setAlbumId(values.getAsInteger("albumId"));
+                    albumSong.setSongId(values.getAsInteger("songId"));
+                    return mMusicDao.updateAlbumSongInfo(albumSong);
+
+                default:
+                    throw new IllegalArgumentException("cant add multiple items");
+            }
+
+        } else {
+            throw new IllegalArgumentException("cant add multiple items");
+        }
+    }
+
+    @Override
+    public int delete(Uri uri, String selection, String[] selectionArgs) {
+
+        switch (URI_MATCHER.match(uri)) {
+            case ALBUM_ROW_CODE:
+                int idAlbum = (int) ContentUris.parseId(uri);
+                return mMusicDao.deleteAlbumById(idAlbum);
+
+            case SONG_ROW_CODE:
+                int idSong = (int) ContentUris.parseId(uri);
+                return mMusicDao.deleteSongById(idSong);
+
+            case ALBUM_SONG_ROW_CODE:
+                int idAlbumSong = (int) ContentUris.parseId(uri);
+                return mMusicDao.deleteAlbumSongById(idAlbumSong);
+
+            default:
+                throw new IllegalArgumentException("cant add multiple items");
+        }
+    }
+
+    private boolean isValuesValid(ContentValues values) {
+        return values.containsKey("id") && values.containsKey("name") && values.containsKey("release");
+    }
+}
---
+++ app/src/main/java/elegion/com/roomdatabase/database/MusicDao.java
@@ -0,0 +1,112 @@
+package elegion.com.roomdatabase.database;
+
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Delete;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.OnConflictStrategy;
+import android.arch.persistence.room.Query;
+import android.arch.persistence.room.Update;
+import android.database.Cursor;
+
+import java.util.List;
+
+/**
+ * @author Azret Magometov
+ */
+
+@Dao
+public interface MusicDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void insertAlbums(List<Album> albums);
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void insertAlbum(Album albums);
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void insertSongs(List<Song> songs);
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void insertSong(Song song);
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void setLinksAlbumSongs(List<AlbumSong> linksAlbumSongs);
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void setLinkAlbumSongs(AlbumSong linkAlbumSongs);
+
+
+    @Query("select * from album")
+    List<Album> getAlbums();
+
+    @Query("select * from album")
+    Cursor getAlbumsCursor();
+
+    @Query("select * from album where id = :albumId")
+    Cursor getAlbumWithIdCursor(int albumId);
+
+
+    @Query("select * from song")
+    List<Song> getSongs();
+
+    @Query("select * from song")
+    Cursor getSongsCursor();
+
+    @Query("select * from song where id = :songId")
+    Cursor getSongsCursorWithIdCursor(int songId);
+
+
+    @Query("select * from albumsong")
+    List<AlbumSong> getAlbumSongs();
+
+    @Query("select * from albumsong")
+    Cursor getAlbumSongsCursor();
+
+    @Query("select * from albumsong where id = :albumSongId")
+    Cursor getAlbumSongsCursorWithIdCursor(int albumSongId);
+
+
+
+    //получить список песен переданного id альбома
+    @Query("select * from song inner join albumsong on song.id = albumsong.song_id where album_id = :albumId")
+    List<Song> getSongsFromAlbum(int albumId);
+
+    //обновить информацию об альбоме
+    @Update
+    int updateAlbumInfo(Album album);
+
+    @Update
+    int updateSongInfo(Song song);
+
+    @Update
+    int updateAlbumSongInfo(AlbumSong albumSong);
+
+    @Delete
+    void deleteAlbum(Album album);
+
+    //удалить альбом по id
+    @Query("DELETE FROM album where id = :albumId")
+    int deleteAlbumById(int albumId);
+
+    @Delete
+    void deleteSong(Song song);
+
+    @Query("delete from song where id = :songId")
+    int deleteSongById(int songId);
+
+    @Delete
+    void deleteAlbumSong(AlbumSong albumSong);
+
+    @Query("delete from albumsong where id = :albumSongId")
+    int deleteAlbumSongById(int albumSongId);
+
+    @Query("delete from album")
+    void deleteAllAlbum();
+
+    @Query("delete from song")
+    void deleteAllSong();
+
+    @Query("delete from albumsong")
+    void deleteAllAlbumSong();
+
+}
